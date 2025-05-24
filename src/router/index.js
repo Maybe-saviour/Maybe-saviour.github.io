59cff2f904357a5f289c227e53f7038931bef9b1
Maybe-saviour <114285903+Maybe-saviour@users.noreply.github.com>
2025-05-24 11:55:50 +0800

    Switch to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import AppHome from '../views/AppHome.vue'; // 修改为多单词格式
+import { createRouter, createWebHashHistory } from 'vue-router'; // 使用 createWebHashHistory
+import AppHome from '../views/AppHome.vue';
 import AboutUs from '../views/AboutUs.vue';
 import SuccessStories from '../views/SuccessStories.vue';
 import ServiceIntroduction from '../views/ServiceIntroduction.vue';
@@ -9,38 +9,29 @@ import Register from '../views/RegisterThe.vue';
 import Login from '../views/LoginThe.vue';
 
 const routes = [
-  { path: '/', component: AppHome },  
+  { path: '/', component: AppHome },
   { path: '/AppHome', component: AppHome },
   { path: '/AboutUs', component: AboutUs },
   { path: '/SuccessStories', component: SuccessStories },
   { path: '/ServiceIntroduction', component: ServiceIntroduction },
   { path: '/NewsThing', component: NewsThing },
   { path: '/ContactUs', component: ContactUs },
-  { path: '/Login', component: Login},
-  { path: '/Register', component: Register},
+  { path: '/Login', component: Login },
+  { path: '/Register', component: Register },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // 使用 hash 模式
   routes,
 });
 
 // 路由守卫
 router.beforeEach((to, from, next) => {
-  // 页面切换时显示加载动画
   const preloader = document.querySelector('.preloader');
   if (preloader) {
-    preloader.classList.remove('hide');  // 显示加载动画
+    preloader.classList.remove('hide');
   }
-  next(); // 继续路由导航
+  next();
 });
-
-// router.afterEach(() => {
-//   // 页面加载完成后隐藏加载动画
-//   const preloader = document.querySelector('.preloader');
-//   if (preloader) {
-//     preloader.classList.add('hide');  // 隐藏加载动画
-//   }
-// });
 
 export default router;
